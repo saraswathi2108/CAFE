39833da82ee047290d9f0fa3b4264fe4738068a2
RamyaYasa <ramyareddy2332@gmail.com>
2026-02-03 14:40:13 +0530

    added excel

--- src/main/java/com/anasol/cafe/dto/ProductBranchOrderMatrixDTO.java
+++ src/main/java/com/anasol/cafe/dto/ProductBranchOrderMatrixDTO.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package com.anasol.cafe.dto;
 
 import lombok.Data;
@@ -13,20 +13,5 @@ public class ProductBranchOrderMatrixDTO {
     private Double warehouseStock;
     private String unit;
     private Map<String, Double> branchOrders = new HashMap<>(); // Key: Branch Name, Value: Total Quantity
-=======
-package com.anasol.cafe.dto;
 
-import lombok.Data;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@Data
-public class ProductBranchOrderMatrixDTO {
-    private Long productId;
-    private String productName;
-    private Double warehouseStock;
-    private String unit;
-    private Map<String, Double> branchOrders = new HashMap<>(); // Key: Branch Name, Value: Total Quantity
->>>>>>> a9f29b546f417df7ae129a5beb0f6d14f264c09e
 }
